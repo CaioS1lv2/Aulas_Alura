4aa32d54cb3ed044e05af72bb19911aeedf72ae9
CaioS1lv2 <164821724+CaioS1lv2@users.noreply.github.com>
2025-07-18 09:06:00 -0300

    Aula Logica de Pro com JS

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function exibirTextoNaTela(tag) {
+    let campo = document.querySelector('tag')
+    campo.innerHTML = Text;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+
+function verificarChute() {
+    console.log('o botão foi clicado!');
+}
